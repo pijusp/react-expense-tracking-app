d7be23cc7cf013231abf76af6c945a01838309a5
Pijus <p.pozera@gmail.com>
2023-02-06 22:40:19 +0200

    First custom component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ExpenseItem() {
+    return (
+        <div>
+            <div>Date</div>
+            <div>
+                <div>
+                    <h2>Car Insurance</h2>
+                </div>
+                <div>$294.67</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
